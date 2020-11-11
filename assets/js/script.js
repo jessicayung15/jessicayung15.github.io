fde50b1092df302667c46d25121aaad0fed8b348
Michael <arbeit@michaelfromage.com>
2020-11-11 16:09:25 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
-$(".ingredients-slider-indicator").click(function () {
+$(".ingredients-slider-indicator").click(function (e) {
+  e.preventDefault();
   var index = $(this).index();
   $(this).parents("#ingredients-slider").find(".slider-item").removeClass("active");
   $(this).parents("#ingredients-slider").find(".slider-item").eq(index).addClass("active");
 
   $(this).parents("#ingredients-slider").find(".ingredients-slider-indicator").removeClass("active");
   $(this).parents("#ingredients-slider").find(".ingredients-slider-indicator").eq(index).addClass("active");
+});
+
+$(".product-slider-indicator").click(function (e) {
+  e.preventDefault();
+  var index = $(this).index();
+  $(this).parents("#product-details-slider").find(".product-slider-item").removeClass("active");
+  $(this).parents("#product-details-slider").find(".product-slider-item").eq(index).addClass("active");
+
+  $(this).parents("#product-details-slider").find(".product-slider-indicator").removeClass("active");
+  $(this).parents("#product-details-slider").find(".product-slider-indicator").eq(index).addClass("active");
+});
+
+$("#hamburger-menu").click(function (e) {
+  e.preventDefault();
+  $("#mobile-menu-container").toggleClass("active");
 });
